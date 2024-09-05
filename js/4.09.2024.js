970a302c48d32b742f5b4c465f39e3b0818d1eb2
Yasir Mehmood <129255451+1Yasir@users.noreply.github.com>
2024-09-05 23:40:57 +0500

    update code

--- js/4.09.2024.js
+++ js/4.09.2024.js
@@ -24,12 +24,10 @@ button.addEventListener("click", function (event) {
 const newTask = document.getElementById("newTask");
 const addTsk = document.getElementById("addTsk");
 const list = document.getElementById("list");
-const tasks = [];
 
 addTsk.addEventListener("click", function () {
     const taskText = newTask.value.trim();
     if (taskText) {
-        tasks.push(taskText);
         newTask.value = "";
         addTaskToList(taskText);
 
@@ -47,34 +45,30 @@ function addTaskToList(taskText) {
         <button class="delete">Delete</button> 
         <button class="edit">Edit</button>
     `;
-    list.appendChild(li); 
+    list.appendChild(li);
 
 }
 
 
-list.addEventListener("click", function(event) {
+list.addEventListener("click", function (event) {
     if (event.target.classList.contains("delete")) {
         // Delete button logic
         const li = event.target.parentElement;
-        const taskText = li.querySelector('span').textContent;
-        
-        tasks.splice(tasks.indexOf(taskText), 1); 
-        li.remove(); 
+        li.remove();
     } else if (event.target.classList.contains("edit")) {
         // Edit button logic
         const li = event.target.parentElement;
         const taskText = li.querySelector('span').textContent;
         const newTaskText = prompt("Edit Task:", taskText);
         if (newTaskText && newTaskText.trim() !== '') {
-            const index = tasks.indexOf(taskText);
-            tasks[index] = newTaskText.trim(); 
-            li.querySelector('span').textContent = newTaskText.trim(); 
+            li.querySelector('span').textContent = newTaskText.trim();
         }
     }
 });
 
 
-tasks.forEach(addTaskToList);
+
+
 
 
 
